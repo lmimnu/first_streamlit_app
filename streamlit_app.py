db7533e9aaa7859eb40840e01fb7b28902029207
lmimnu <150334592+lmimnu@users.noreply.github.com>
2023-11-09 03:34:11 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,9 @@
 import streamlit
 
 streamlit.title("My parents new healthy diner")
+
+streamlit.header("break fast menu list")
+
+Streamlit.text("Omega3 & Blueberry oatmeal")
+Streamlit.text("Kale, Spinach & Rocket Smoothhie")
+Streamlit.text("Hard-Boiled Free-Range Egg")
