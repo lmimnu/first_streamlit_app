d78260bea4f61d3ba71939a3d2aa9eed92c72c6d
lmimnu <150334592+lmimnu@users.noreply.github.com>
2023-11-09 03:40:47 -0600

    Update streamlit_app.py
    updated list

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,8 +2,9 @@ import streamlit
 
 streamlit.title("My parents new healthy diner")
 
-streamlit.header("break fast menu list")
+streamlit.header("BreakFast favourites")
 
-streamlit.text('Omega3 & Blueberry oatmeal')
-streamlit.text('Kale, Spinach & Rocket Smoothhie')
-streamlit.text("Hard-Boiled Free-Range Egg")
+streamlit.text('🥣 Omega3 & Blueberry oatmeal')
+streamlit.text(' 🥗 Kale, Spinach & Rocket Smoothhie')
+streamlit.text("🐔 🥑🍞Hard-Boiled Free-Range Egg")
+streamlit.text("🥑🍞 Avocado Toast")
