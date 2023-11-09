d34a67b533144cb7ec6d64bc49cc3cfc617855c6
lmimnu <150334592+lmimnu@users.noreply.github.com>
2023-11-09 03:35:52 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,6 +4,6 @@ streamlit.title("My parents new healthy diner")
 
 streamlit.header("break fast menu list")
 
-Streamlit.text("Omega3 & Blueberry oatmeal")
-Streamlit.text("Kale, Spinach & Rocket Smoothhie")
+Streamlit.text('Omega3 & Blueberry oatmeal')
+Streamlit.text('Kale, Spinach & Rocket Smoothhie')
 Streamlit.text("Hard-Boiled Free-Range Egg")
