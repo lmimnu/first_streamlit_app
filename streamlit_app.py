aa86f60ab1a58a0ab4530626e7d764bb67f4e90b
lmimnu <150334592+lmimnu@users.noreply.github.com>
2023-11-09 03:37:27 -0600

    Update streamlit_app.py
    lower case update

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,6 +4,6 @@ streamlit.title("My parents new healthy diner")
 
 streamlit.header("break fast menu list")
 
-Streamlit.text('Omega3 & Blueberry oatmeal')
-Streamlit.text('Kale, Spinach & Rocket Smoothhie')
-Streamlit.text("Hard-Boiled Free-Range Egg")
+streamlit.text('Omega3 & Blueberry oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothhie')
+streamlit.text("Hard-Boiled Free-Range Egg")
